f1cc33b13569259690691ab787f3f4ae3e370fee
rwaynewhite <78130055+rwaynewhite15@users.noreply.github.com>
2021-06-30 22:10:41 -0400

    initial routes created

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const fs = require('fs');
+const { post } = require('../htmlRoutes');
+
+function getNotes(){
+    const notes = fs.readFileSync("db/db.json", "utf8");
+    return JSON.parse(notes);
+}
+function saveNotes(notes){
+    fs.writeFileSync("db/db.json", JSON.stringify(notes), "utf8");
+}
+
+router.get("/notes", (req, res) => {
+    const notes = getNotes();
+    res.json(notes);
+  });
+
+router.post("/notes",(req,res) => {
+    const note = req.body;
+    const notes = getNotes();
+    notes.push(note);
+    saveNotes(notes);
+    res.json(note)
+})
+
+
+module.exports = router;
+
