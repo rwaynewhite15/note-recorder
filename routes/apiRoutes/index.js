4fc0d3f38dfb47a4080d1a9b07b5faa650abd484
rwaynewhite <78130055+rwaynewhite15@users.noreply.github.com>
2021-08-04 00:21:35 -0400

    Finished Delete Route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const fs = require('fs');
 const { post } = require('../htmlRoutes');
+let uniqueID = 1
 
 function getNotes(){
     const notes = fs.readFileSync("db/db.json", "utf8");
@@ -16,12 +17,23 @@ router.get("/notes", (req, res) => {
     res.json(notes);
   });
 
+router.delete("/notes/:id", (req, res) => {
+    const notes = getNotes();
+    const unpdatedNotes = notes.filter(note => note.id !== req.params.id);
+    saveNotes(unpdatedNotes);
+    res.status.json({
+        ok : true
+    });
+  });
+
 router.post("/notes",(req,res) => {
     const note = req.body;
+    const {title,text} = note;
+    const newNote = {title,text,id:(uniqueID++).toString()};
     const notes = getNotes();
-    notes.push(note);
+    notes.push(newNote);
     saveNotes(notes);
-    res.json(note)
+    res.json(newNote);
 })
 
 module.exports = router;
